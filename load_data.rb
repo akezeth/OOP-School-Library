15ec2c167248ec6c85c97c6305c42d0ac0805b7c
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-08-08 16:45:27 +0100

    ADD method to save people

--- load_data.rb
+++ load_data.rb
@@ -6,6 +6,7 @@ require_relative 'rental'
 
 module LoadData
   BOOKS_FILE_NAME = './books.json'.freeze
+  PEOPLE_FILE_NAME = './people.json'.freeze
   
 
   def load_books
@@ -17,5 +18,26 @@ module LoadData
     @books = books_list.map do |book|
       Book.new(book['title'], book[author])
     end
-  end  
+  end
+
+  def load_people
+    people_list = []
+    return people_list unless File.exist?(PEOPLE_FILE_NAME)
+
+    people_list = load_data_from_file(PEOPLE_FILE_NAME)
+    people_list.each do |person|
+      if person['title'] == 'Student'
+        student = Student.new(Classroom.new(person['classroom']), person['age'], person['name'], parent_permission: person['parent_permission'])
+        student.id = person['id']
+        @persons << student
+      else
+        teacher = Teacher.new(person['specialization'], person['age'], person['name'])
+        teacher.id = person['id']
+        @persons << teacher
+      end
+    end
+  end
+
 end
+
+  
