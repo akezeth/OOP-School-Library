75a6df8d5afcc6f5ba2872a7ad945aa51ad4f455
akezeth <116703083+akezeth@users.noreply.github.com>
2023-08-09 12:12:44 +0300

    Change @people to @persons

--- preserve_data.rb
+++ preserve_data.rb
@@ -10,6 +10,7 @@ module PreserveData
   def save_to_file(file_name, data)
     File.write(file_name, JSON.pretty_generate(data))
   end
+
   def save_books
     books_list = @books.map do |book|
       {
@@ -19,38 +20,45 @@ module PreserveData
     end
     save_to_file(BOOKS_FILE_NAME, books_list)
   end
+
   def save_people
-    people_list = @people.map do |person|
+    people_list = @persons.map do |person|
       if person.title == 'Student'
         {
-          id: person.id.to_s, name: person.name, age: person.age,
-          parent_permission: person.parent_permission, classroom: person.classroom.label,
+          id: person.id.to_s,
+          name: person.name, age: person.age,
+          parent_permission: person.parent_permission,
+          classroom: person.classroom,
           title: person.title
         }
       else
         {
-          id: person.id.to_s, name: person.name, age: person.age,
-          specialization: person.specialization, title: person.title
+          id: person.id.to_s,
+          name: person.name,
+          age: person.age,
+          specialization: person.specialization,
+          title: person.title
         }
       end
     end
     save_to_file(PEOPLE_FILE_NAME, people_list)
   end
+
   def save_rentals
     rentals_list = []
     @rentals.each do |rental|
       rental_item = {
-        date: rental.date,
+        date: rental.date, person: {
+                             id: rental.person.id, name: rental.person.name,
+                             age: rental.person.age, title: rental.person.title
+                           },
         book: {
           title: rental.book.title, author: rental.book.author
-        },
-        person: {
-          id: rental.person.id, name: rental.person.name, age: rental.person.age, title: rental.person.title
         }
       }
       if rental.person.title == 'Student'
-        rental_item[:person][:classroom] = rental.person.classroom.label
         rental_item[:person][:parent_permission] = rental.person.parent_permission
+        rental_item[:person][:classroom] = rental.person.classroom
       else
         rental_item[:person][:specialization] = rental.person.specialization
       end
