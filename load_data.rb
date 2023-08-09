fdf644ae413b26995559f0d474fcb3de106e6309
akezeth <116703083+akezeth@users.noreply.github.com>
2023-08-09 12:12:14 +0300

    Change the order of parameters in load_rentals

--- load_data.rb
+++ load_data.rb
@@ -9,6 +9,11 @@ module LoadData
   PEOPLE_FILE_NAME = './people.json'.freeze
   RENTALS_FILE_NAME = './rentals.json'.freeze
 
+  def load_data_from_file(file_name)
+    file = File.read(file_name)
+    JSON.parse(file)
+  end
+
   def load_books
     books_list = []
     return books_list unless File.exist?(BOOKS_FILE_NAME)
@@ -16,7 +21,7 @@ module LoadData
     books_list = load_data_from_file(BOOKS_FILE_NAME)
 
     @books = books_list.map do |book|
-      Book.new(book['title'], book[author])
+      Book.new(book['title'], book['author'])
     end
   end
 
@@ -27,11 +32,12 @@ module LoadData
     people_list = load_data_from_file(PEOPLE_FILE_NAME)
     people_list.each do |person|
       if person['title'] == 'Student'
-        student = Student.new(Classroom.new(person['classroom']), person['age'], person['name'], parent_permission: person['parent_permission'])
+        student = Student.new(classroom: Classroom.new(person['classroom']), age: person['age'], name: person['name'],
+                              parent_permission: person['parent_permission'])
         student.id = person['id']
         @persons << student
       else
-        teacher = Teacher.new(person['specialization'], person['age'], person['name'])
+        teacher = Teacher.new(specialization: person['specialization'], age: person['age'], name: person['name'])
         teacher.id = person['id']
         @persons << teacher
       end
@@ -46,25 +52,20 @@ module LoadData
     rentals_list.each do |rental|
       book = Book.new(rental['book']['title'], rental['book']['author'])
       person = create_rental_person(rental['person'])
-      @rentals << Rental.new(rental['date'], book, person)
+      @rentals << Rental.new(rental['date'], person, book)
     end
   end
 
   def create_rental_person(person)
     if person['title'] == 'Student'
-      student = Student.new(Classroom.new(person['classroom']), person['age'], person['name'], parent_permission: person['parent_permission'])
+      student = Student.new(classroom: Classroom.new(person['classroom']), age: person['age'], name: person['name'],
+                            parent_permission: person['parent_permission'])
       student.id = person['id'].to_i
       student
     else
-      teacher = Teacher.new(person['specialization'], person['age'], person['name'])
+      teacher = Teacher.new(specialization: person['specialization'], age: person['age'], name: person['name'])
       teacher.id = person['id'].to_i
       teacher
     end
   end
-  
-  def load_data_from_file(file_name)
-    file = File.read(file_name)
-    JSON.parse(file)
-  end
 end
-  
