72458623b7d4480587a3ee00b4e761a0a32a4b2b
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-09 23:46:38 +0200

    Modify classroom.rb test

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,20 +1,27 @@
-require_relative '../classroom'
+require_relative '../classroom' # Make sure the paths are correct
+require_relative '../student' # Make sure the paths are correct
 
-RSpec.describe Classroom do
-  let(:classroom) { Classroom.new('101') }
-  let(:student) { Student.new(classroom: classroom, age: 19, name: 'John') }
-  # let(:classroom_student) { Classroom.new('101').add_student(student) }
-  describe 'initialize' do
-    it 'creates a new classroom' do
-      expect(classroom.label).to eq('101')
-      expect(classroom.instance_variable_get(:@students)).to eq([])
+describe Classroom do
+  let(:classroom) { Classroom.new('Classroom 101') }
+
+  describe '#initialize' do
+    it 'initializes a classroom with the provided label' do
+      expect(classroom.label).to eq('Classroom 101')
+    end
+
+    it 'initializes a classroom with an empty students list' do
+      expect(classroom.students).to be_empty
     end
   end
 
-  describe 'Add students' do
-    it 'Adds a new student' do
-      classroom_student = classroom.add_students(student)
-      expect(classroom_student.students.length).to eq(1)
+  describe '#add_student' do
+    it 'adds a student to the classroom' do
+      student = instance_double('Student')
+      expect(student).to receive(:classroom=).with(classroom)
+
+      classroom.add_student(student)
+
+      expect(classroom.students).to include(student)
     end
   end
 end
