689eb21ce13a1fadcf869d05847734c9017d452e
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-08-09 16:34:37 +0100

    ADD test for book.rb

---
+++ spec/book_spec.rb
@@ -0,0 +1,22 @@
+require 'date'
+require_relative '../book'
+
+RSpec.describe Book do
+ book = Book.new('A song of ice and fire', 'George R. R. Martin')
+  describe 'initialize' do
+    it 'creates a new book' do
+      expect(book.title).to eq('A song of ice and fire')
+      expect(book.author).to eq('George R. R. Martin')
+      expect(book.instance_variable_get(:@rentals)).to eq([])
+    end
+  end
+
+  describe 'Add rental' do
+    it 'Adds a new rental' do
+      person = Person.new(age: 19, name: 'John')
+      date = Date.today
+      rental = book.add_rental(person, date)
+      expect(rental.date).to eq(Date.today)
+    end
+  end
+end 
