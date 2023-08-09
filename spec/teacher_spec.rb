9fce28010e83657b8192f6f09621f5040cf54b18
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-09 16:29:45 +0200

    Add tests for teacher.rb

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../teacher.rb'
+require_relative '../person.rb'  
+
+RSpec.describe Teacher do
+  subject do
+    described_class.new(
+      specialization: 'Mathematics',
+      age: 30,
+      name: 'Jane Smith'
+    )
+  end
+
+  describe "#initialize" do
+    it "sets the attributes correctly" do
+      expect(subject.age).to eq(30)
+      expect(subject.name).to eq('Jane Smith')
+      expect(subject.instance_variable_get(:@parent_permission)).to eq(true)
+      expect(subject.specialization).to eq('Mathematics')
+      expect(subject.title).to eq('Teacher')
+    end
+  end
+
+  describe "#can_use_services?" do
+    it "returns true" do
+      expect(subject.can_use_services?).to eq(true)
+    end
+  end  
+end
